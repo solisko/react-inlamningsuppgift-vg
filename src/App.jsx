eb210c2ab2a594902b4a6b2a3a1623e9b7a40314
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-01 10:50:10 +0200

    context in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./Context/ShopContextProvider";
 import Navigation from "./components/Header/Navigation";
 import Switch from "./components/Header/Switch";
 import Footer from "./components/Footer/Footer";
@@ -7,11 +8,13 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navigation/>
-        <Switch />
-      </BrowserRouter>
-      <Footer />
+      <ShopContextProvider>
+        <BrowserRouter>
+          <Navigation />
+          <Switch />
+        </BrowserRouter>
+        <Footer />
+      </ShopContextProvider>
     </div>
   );
 }

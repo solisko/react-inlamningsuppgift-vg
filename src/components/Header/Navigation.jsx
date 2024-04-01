bb7a815bb5db320dd8edf8e29854bcab1c7e46b8
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-01 11:13:55 +0200

    header cont

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,28 +1,32 @@
 import { NavLink } from "react-router-dom";
 import styles from "../Header/header.module.css";
-import cart from "../Assets/cart.png"
+import cart from "../Assets/cart.png";
 
 const Navigation = () => {
   return (
     <nav className={styles.navBar}>
-      <div className={siteNameDiv} >
-
-      <h1>YarnHub</h1>
+      <div className={styles.siteLogoDiv}>
+        <h1>YarnHub</h1>
       </div>
-      <div className={styles.navLinksDiv}>
-        <span>
+      <ul className={styles.linksDiv}>
+        <li>
           <NavLink to="/">Hem</NavLink>
-        </span>
-        <button>
-          <NavLink to="/create">Skapa konto</NavLink>
-        </button>
-        <button>
-          <NavLink to="/login">Logga in</NavLink>
-        </button>
-        <div className={styles.cartDiv}></div>
-        <span>
-          <NavLink to="/basket"><img src={cart} alt="" /></NavLink>
-        </span>
+        </li>
+        <li>
+          <button>
+            <NavLink to="/create">Skapa konto</NavLink>
+          </button>
+        </li>
+        <li>
+          <button>
+            <NavLink to="/login">Logga in</NavLink>
+          </button>
+        </li>
+      </ul>
+      <div className={styles.cartDiv}>
+        <NavLink to="/basket">
+          <img src={cart} alt="" />
+        </NavLink>
       </div>
     </nav>
   );

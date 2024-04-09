840c01d68ada40c5e70bb1cc26bd9eba11797309
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-09 13:32:00 +0200

    testing

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res)=>{
+    res.send("hello world")
+})
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });

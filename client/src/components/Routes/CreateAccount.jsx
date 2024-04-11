35eb588e4c3e7a5d5ef1f29766c4911a6f2019d1
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-11 14:32:15 +0200

    create account works

--- client/src/components/Routes/CreateAccount.jsx
+++ client/src/components/Routes/CreateAccount.jsx
@@ -1,61 +1,3 @@
-// import { useState } from "react";
-
-// export default function CreateAccount() {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleCreate = async (username, password) => {
-//     try {
-//       const response = await fetch("http://localhost:3000/accounts", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ username, password }),
-//       });
-//       if (!response.ok) {
-//         const errorMessage = await response.text();
-//         throw new Error(`Failed to create user: ${errorMessage}`);
-//       }
-//       console.log("Auction created successfully!");
-//       setUsername("");
-//       setPassword("");
-
-//       const data = await response.json();
-//       console.log('User created successfully:', data);
-//     } catch (error) {
-//       console.error('Error creating user:', error.message);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div>
-//         <label htmlFor="username">Användarnamn/Epost</label>
-//         <input
-//           type="text"
-//           placeholder="Användarnamn"
-//           value={username}
-//           onChange={(e) => setUsername(e.target.value)}
-//           required
-//         />
-//         <label htmlFor="password">Lösenord</label>
-//         <input
-//           type="text"
-//           placeholder="Lösenord"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           required
-//         />
-//         <button type="button" onClick={handleCreate}>
-//           Skapa konto
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import { useState } from "react";
 
 export default function CreateAccount() {
@@ -75,7 +17,7 @@ export default function CreateAccount() {
         const errorMessage = await response.text();
         throw new Error(`Failed to create user: ${errorMessage}`);
       }
-      console.log("User created successfully!");
+      console.log("Account created successfully!");
       setUsername("");
       setPassword("");
     } catch (error) {

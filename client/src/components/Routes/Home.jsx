af00346e68b1c868c4323108c2653f71b913fa16
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-11 22:53:31 +0200

    horizontal scroll

--- client/src/components/Routes/Home.jsx
+++ client/src/components/Routes/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContextProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./home.module.css";
 
 const Home = () => {
@@ -12,11 +13,13 @@ const Home = () => {
       {categories.map((category, categoryIndex) => (
         <div className={styles.container} key={categoryIndex}>
           <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
-          <div className={styles.categoryWrapper}>
+          <div
+            className={`${styles.categoryWrapper} row flex-nowrap overflow-auto`}
+          >
             {getProductsByCategory(category).map((product, index) => (
-              <div className={styles.productCard} key={index}>
+              <div className={`${styles.productCard} col-sm-4 p-0`} key={index}>
                 <img className={styles.image} src="" alt="" />
-                <section className={styles.namePriceSection} >
+                <section className={styles.namePriceSection}>
                   <h3>{product.yarnName}</h3>
                   <p>{product.yarnPrice}</p>
                 </section>

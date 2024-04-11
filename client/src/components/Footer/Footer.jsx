cc21aac09696c684a85ce571e37eec113dd8f083
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-11 23:02:31 +0200

    footer fixxxx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "../Footer/footer.module.css"
 
 export default function Footer() {
   return (
-    <div className={styles.footerContainer} >
+    <div className={`${styles.footerContainer} mt-auto`}>
         <span>Adress: Ingenstans 8, 987 65 Järbo </span>
         <span>Kontakt: 0765 74 83 </span>
         <span>Trustpilot</span>

3ba635fa62e758c4372486082c80af2ecbdc74f3
Sofia Skommar <sofiaskommar@gmail.com>
2024-04-13 22:25:41 +0200

    add and remove

--- client/src/components/CartItems/CartItems.jsx
+++ client/src/components/CartItems/CartItems.jsx
@@ -1,7 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useContext } from "react";
+import { ShopContext } from "../../Context/ShopContextProvider";
 import styles from "../CartItems/cartitems.module.css";
 
 export default function CartItems({ product }) {
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+
   return (
     <div className={`${styles.productCard} col-sm-4 p-0`}>
       <img className={styles.image} src="" alt="" />
@@ -9,6 +13,11 @@ export default function CartItems({ product }) {
         <h3>{product.yarnName}</h3>
         <p>{product.yarnPrice}</p>
       </section>
+      <div>
+        <button onClick={() => removeFromCart(product.yarnID)} >-</button>
+        <input type="text" value={cartItems[product.yarnID]} onChange={(e => {})} />
+        <button onClick={() => addToCart(product.yarnID)} >-</button>
+      </div>
     </div>
   );
 }
